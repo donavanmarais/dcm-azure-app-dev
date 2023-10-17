ee850475205639b4e7b1e45340d6cfa6f80991bf
donma <donma@DONAVANMARAIS>
2023-10-17 14:37:15 -0400

    Add project files.

---
+++ dcm-azure-app-dev/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dcm_azure_app_dev.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
